4926b10600826e79dee88d018bc0adacf731e40f
Ricardo Guerra <RikkuG491@gmail.com>
2023-07-27 10:49:40 -0400

    Completed Phase 4 example project - hotels database

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,112 @@
+import '../index.css';
+
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import NavBar from './NavBar'
+import Header from './Header'
+import HotelList from './HotelList'
+import NewHotelForm from './NewHotelForm'
+import UpdateHotelForm from './UpdateHotelForm'
 
 function App() {
-  return <h1>Phase 4 Project Client</h1>;
+
+  const [hotels, setHotels] = useState([])
+  const [postFormData, setPostFormData] = useState({})
+  const [idToUpdate, setIdToUpdate] = useState(0)
+  const [patchFormData, setPatchFormData] = useState({})
+
+  useEffect(() => {
+    fetch('/hotels')
+    .then(response => response.json())
+    .then(hotelData => setHotels(hotelData))
+  }, [])
+
+  useEffect(() => {
+    if(hotels.length > 0 && hotels[0].id){
+      setIdToUpdate(hotels[0].id)
+    }
+  }, [hotels])
+
+  function addHotel(event){
+    event.preventDefault()
+
+    fetch('/hotels', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(postFormData)
+    })
+    .then(response => response.json())
+    .then(newHotel => setHotels(hotels => [...hotels, newHotel]))
+  }
+
+  function updateHotel(event){
+    event.preventDefault()
+    fetch(`/hotels/${idToUpdate}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(patchFormData)
+    })
+    .then(response => response.json())
+    .then(updatedHotel => {
+      setHotels(hotels => {
+        return hotels.map(hotel => {
+          if(hotel.id === updatedHotel.id){
+            return updatedHotel
+          }
+          else{
+            return hotel
+          }
+        })
+      })
+    })
+  }
+
+  function deleteHotel(id){
+    fetch(`/hotels/${id}`, {
+      method: "DELETE"
+    })
+    .then(() => setHotels(hotels => {
+      return hotels.filter(hotel => {
+        return hotel.id !== id
+      })
+    }))
+  }
+
+  function updatePostFormData(event){
+    setPostFormData({...postFormData, [event.target.name]: event.target.value})
+  }
+
+  function updatePatchFormData(event){
+    setPatchFormData({...patchFormData, [event.target.name]: event.target.value})
+  }
+
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <NavBar/>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <h1>Welcome! Here is the list of hotels available:</h1>
+            <HotelList hotels={hotels} deleteHotel={deleteHotel}/>
+          </Route>
+          <Route path="/add_hotel">
+            <NewHotelForm addHotel={addHotel} updatePostFormData={updatePostFormData}/>
+          </Route>
+          <Route path="/update_hotel">
+            <UpdateHotelForm updateHotel={updateHotel} setIdToUpdate={setIdToUpdate} updatePatchFormData={updatePatchFormData} hotels={hotels}/>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ server/migrations/versions/c907cb195bed_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: c907cb195bed
+Revises: 
+Create Date: 2023-07-26 16:30:19.582075
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c907cb195bed'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
